8ffa77a63fc941b301754174803c9c2b49736f84
Antoine Castel <castel.antoine95@gmail.com>
2023-05-13 18:16:49 +0300

    port

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def main() -> None:
 
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
-                          port=int(os.environ.get('PORT', 5000)),
+                          port=int(os.environ.get('PORT', 80)),
                           #url_path=telegram_bot_token,
                           #webhook_url=+ telegram_bot_token
                           )
