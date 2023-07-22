4ba109bd228a8d5291e93d78ba493657fb5b4195
Antoine Castel <31851170+TheFreshPenguin@users.noreply.github.com>
2023-07-22 14:04:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ def birthday(update: Update, context: CallbackContext) -> None:
     )
 
 def start_scheduler(update: Update, context: CallbackContext) -> None:
-    context.job_queue.run_daily(birthday, time=time(hour=14, minute=5), context = -961065253)
+    context.job_queue.run_daily(birthday, time=time(hour=14, minute=7), context=update.message.chat_id)
 
 # Add a command handler to stop the daily task (optional)
 # def stop(update, context):
